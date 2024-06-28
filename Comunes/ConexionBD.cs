da80ec625f9a3e79e40bb56270264765adb3c32f
Remar - Eduardo Manosalvas <emanosalvas@remar.com.ec>
2024-06-27 21:20:13 -0500

    Creación de Método Get

---
+++ Comunes/ConexionBD.cs
@@ -0,0 +1,49 @@
+﻿using _01Usuarios___EMANOSALVAS.Modelos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _01Usuarios___EMANOSALVAS.Comunes
+{
+    public static class ConexionBD
+    {
+        public static SqlConnection conexion;
+
+       public static SqlConnection abrirConexion()
+        {
+            //conexion = new SqlConnection("Server=LT-EMANOSALVAS\\SQLEXPRESS;Database=PROYECTO_1;Trusted_Connection=True;");
+            conexion = new SqlConnection("Server=LT-EMANOSALVAS\\SQLEXPRESS;Database=PROYECTO_1;User Id=sa;Password=Representaciones.2024;");
+            conexion.Open();
+            return conexion;
+        }
+
+        public static List<Usuario> GetUsuarios()
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType= System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_GET_USUARIOS";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ds);
+            return fillUsuarios(ds);
+
+        }
+
+        private static List<Usuario> fillUsuarios(DataSet ds)
+        {
+            List<Usuario> lrespuesta = new List<Usuario>();
+            Usuario objUsuario= new Usuario();
+            for (int i = 0; i <= ds.Tables[0].Rows.Count-1; i++)
+            {
+                objUsuario = new Usuario();
+                objUsuario.id_usuario =Convert.ToInt32(ds.Tables[0].Rows[i]["ID_USUARIO"].ToString());
+                objUsuario.codigo = ds.Tables[0].Rows[i]["CODIGO"].ToString(); 
+                objUsuario.nombres = ds.Tables[0].Rows[i]["NOMBRES"].ToString();
+                objUsuario.apellidos = ds.Tables[0].Rows[i]["APELLIDOS"].ToString();
+                lrespuesta.Add(objUsuario);
+            }
+            return lrespuesta; 
+        }
+    }
+}
---
+++ Modelos/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace _01Usuarios___EMANOSALVAS.Modelos
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public string codigo { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public string contrasena { get; set; }
+        public string estado { get; set; }
+        public DateTime fecha_ultima_conexion { get; set; }
+        public int usuario_creacion { get; set; }
+        public DateTime fecha_creacion { get; set; }
+        public int usuario_modificacion { get; set; }
+        public DateTime fecha_modificacion { get; set; }
+
+    }
+}
